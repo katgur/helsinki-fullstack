a6e1d65d957a3ef4d0fb5b62df842dc485e6f8f5
katgur <mitsutanaka29@gmail.com>
2023-09-05 23:16:47 +0300

    task 1.7

--- part1/unicafe/src/component/Statistics.jsx
+++ part1/unicafe/src/component/Statistics.jsx
@@ -1,12 +1,37 @@
 function Statistics({ data }) {
+    const calculateAll = () => {
+        return Object.entries(data).reduce((acc, entry) => acc + entry[1], 0)
+    }
+
+    const calculateAverage = () => {
+        const count = calculateAll()
+        if (count == 0) {
+            return 0;
+        }
+        return (data.good - data.bad) / count
+    }
+
+    const calculatePositive = () => {
+        const count = calculateAll()
+        if (count == 0) {
+            return 0;
+        }
+        return data.good / count
+    }
+
     return (
         <section>
             <h1>statistics</h1>
-            {
-                Object.entries(data).map(([key, value]) => {
-                    return <p key={key}>{key} {value}</p>
-                })
-            }
+            <ul>
+                {
+                    Object.entries(data).map(([key, value]) => {
+                        return <li key={key}>{key} {value}</li>
+                    })
+                }
+                <li>all {calculateAll()}</li>
+                <li>average {calculateAverage().toFixed(2)}</li>
+                <li>positive {calculatePositive().toFixed(2)}%</li>
+            </ul>
         </section>
     )
 }
