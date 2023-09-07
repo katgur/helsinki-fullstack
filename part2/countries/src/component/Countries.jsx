8f348166af9ccef8f6946d875bbbb9d0492de812
katgur <mitsutanaka29@gmail.com>
2023-09-08 00:22:04 +0300

    2.19*: Data for countries, step2

--- part2/countries/src/component/Countries.jsx
+++ part2/countries/src/component/Countries.jsx
@@ -2,59 +2,89 @@ import { useEffect } from "react"
 import { useState } from "react"
 import countryService from '../service/countries'
 
+function Country({ country }) {
+    return (
+        country &&
+        <div>
+            <h1>{country.name}</h1>
+            <p>capital {country.capital}</p>
+            <p>area {country.area}</p>
+            <h3>languages:</h3>
+            <ul>
+                {
+                    country.languages.map(language => {
+                        return <li key={language}>{language}</li>
+                    })
+                }
+            </ul>
+            <img src={country.flag} alt={country.alt} />
+        </div>
+    )
+}
+
 function Countries({ countries, search }) {
     const [country, setCountry] = useState(null)
+    const [selected, setSelected] = useState(-1)
+    const [mode, setMode] = useState()
 
-    const filtered = countries.filter(country => {
-        return country.toLowerCase().includes(search)
-    })
+    function getCountry(name) {
+        countryService.getByName(name)
+            .then(data => {
+                setCountry({ name: data.name.common, capital: data.capital[0], area: data.area, languages: Object.values(data.languages), flag: data.flags.png, alt: data.flags.alt })
+            })
+    }
 
     useEffect(() => {
-        if (filtered.length !== 1) {
-            if (country) {
-                setCountry(null)
-            }
-            return
+        const filtered = countries.filter(country => {
+            return country.toLowerCase().includes(search)
+        })
+
+        if (filtered.length > 10) {
+            setMode(0)
+        } else if (filtered.length > 1) {
+            setMode(1)
+        } else {
+            setMode(2)
+            getCountry(filtered[0])
         }
-        if (!country) {
-            countryService.getByName(filtered[0])
-                .then(data => {
-                    setCountry({ name: data.name.common, capital: data.capital[0], area: data.area, languages: Object.values(data.languages), flag: data.flags.png, alt: data.flags.alt })
-                })
+    }, [countries, search, selected])
+
+    useEffect(() => {
+        const filtered = countries.filter(country => {
+            return country.toLowerCase().includes(search)
+        })
+        if (selected !== -1) {
+            getCountry(filtered[selected])
         }
-    }, [filtered])
+    }, [selected])
+
+    const onShowButtonClick = (index) => {
+        setSelected(index)
+    }
 
     return (
         <>
+            {mode === 0 && <span>Too many countries, specify another filter</span>}
             {
-                filtered.length > 10 && <span>Too many countries, specify another filter</span>}
-            {
-                filtered.length > 1 && filtered.length <= 10 &&
+                mode === 1 &&
                 <ul>
                     {
-                        filtered.map(country => {
-                            return <li key={country}>{country}</li>
-                        })
+                        countries
+                            .filter(country => {
+                                return country.toLowerCase().includes(search)
+                            })
+                            .map((fCountry, index) => {
+                                return (
+                                    <li key={fCountry}>
+                                        <p>{fCountry} <button onClick={() => onShowButtonClick(index)}>show</button></p>
+                                        {selected === index && <Country country={country} />}
+                                    </li>
+                                )
+                            })
                     }
                 </ul>
             }
-            {
-                country &&
-                <div>
-                    <h1>{country.name}</h1>
-                    <p>capital {country.capital}</p>
-                    <p>area {country.area}</p>
-                    <h3>languages:</h3>
-                    <ul>
-                        {
-                            country.languages.map(language => {
-                                return <li key={language}>{language}</li>
-                            })
-                        }
-                    </ul>
-                    <img src={country.flag} alt={country.alt} />
-                </div>
-            }
+            {mode === 2 && <Country country={country} />}
         </>
     )
 }
