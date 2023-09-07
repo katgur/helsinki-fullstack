cde3b679e6f8438c617598dc8a12bae8a6351960
katgur <mitsutanaka29@gmail.com>
2023-09-07 20:56:48 +0300

    2.16: Phonebook step11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,10 +3,12 @@ import Filter from './component/Filter'
 import PersonForm from './component/PersonForm'
 import Persons from './component/Persons'
 import personsService from './service/persons'
+import Alert from './component/Alert'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [search, setSearch] = useState('')
+  const [message, setMessage] = useState(null)
 
   useEffect(() => {
     personsService.getAll()
@@ -15,19 +17,34 @@ const App = () => {
       })
   }, [])
 
+  function showMessage(text, type) {
+    setMessage({ text, type })
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
+
   const onAddNewPersonClick = (data, reset) => {
     const sameIndex = persons.findIndex(person => person.name === data.name)
     if (sameIndex !== -1) {
       if (window.confirm(`Person with name '${data.name}' is already added to phonebook, replace the old number with the new one ?`)) {
         personsService.update(persons[sameIndex].id, data)
-        .then(data => {
-          const newPersons = persons.filter(person => person.id !== data.id)
-          setPersons([...newPersons, data])
-        })
+          .then(data => {
+            showMessage(`Person '${data.name}' is successfully updated`, 'success')
+            const newPersons = persons.filter(person => person.id !== data.id)
+            setPersons([...newPersons, data])
+            reset()
+          })
+          .catch(error => {
+            showMessage(`Person '${data.name}' was already removed from server`, 'error')
+            const newPersons = persons.filter(person => person.id !== persons[sameIndex].id)
+            setPersons(newPersons)
+          })
       }
     } else {
       personsService.create(data)
         .then(data => {
+          showMessage(`Person '${data.name}' is successfully added`, 'success')
           setPersons([...persons, data])
           reset()
         })
@@ -38,6 +55,7 @@ const App = () => {
     if (window.confirm(`Delete ${data.name} ?`)) {
       personsService.remove(data.id)
         .then(_ => {
+          showMessage(`Person'${data.name}' is successfully deleted`, 'success')
           const newPersons = persons.filter(person => person.id !== data.id)
           setPersons(newPersons)
         })
@@ -47,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Alert message={message} />
       <Filter search={search} setSearch={setSearch} />
       <h3>Add a new person</h3>
       <PersonForm onSubmit={onAddNewPersonClick} />
