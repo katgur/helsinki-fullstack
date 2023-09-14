7f180324cad2858ee203e6ee2febe4646acda9d3
katgur <mitsutanaka29@gmail.com>
2023-09-14 18:18:05 +0300

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/util/list_helper.js
+++ part4/bloglist/util/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, blog) => acc + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return undefined
+    }
+    let max = blogs[0]
+    blogs.forEach(blog => {
+        if (blog.likes > max.likes) {
+            max = blog
+        }
+    })
+    return { title: max.title, author: max.author, likes: max.likes }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
