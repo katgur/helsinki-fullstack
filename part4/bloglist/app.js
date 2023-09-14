fd75c03dbeb8099aba8d5443b832d19170bfc5e7
katgur <mitsutanaka29@gmail.com>
2023-09-14 17:38:03 +0300

    4.2 Blog list, step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,33 @@
+const config = require('./util/config')
+const express = require('express')
+const cors = require('cors')
+const blogRouter = require('./controller/blogs')
+const middleware = require('./util/middleware')
+const logger = require('./util/logger')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+
+logger.info('Connecting to MongoDB')
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
