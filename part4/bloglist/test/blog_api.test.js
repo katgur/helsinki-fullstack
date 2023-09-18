abbb5464c79322151a3f886aee4a53b14134ddff
katgur <mitsutanaka29@gmail.com>
2023-09-18 16:53:57 +0300

    4.10: Blog list tests, step3

--- part4/bloglist/test/blog_api.test.js
+++ part4/bloglist/test/blog_api.test.js
@@ -27,13 +27,30 @@ test('blogs are returned as json', async () => {
 test('there are two blogs', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(2)
-})
+}, TIMEOUT)
 
 test('unique identifier is called id', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body[0].id).toBeDefined()
     expect(response.body[1].id).toBeDefined()
-})
+}, TIMEOUT)
+
+test('blog is added', async () => {
+    const newBlog = initialBlogs[2]
+
+    const result = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    expect(result._body.title).toBe(newBlog.title)
+    expect(result._body.author).toBe(newBlog.author)
+    expect(result._body.url).toBe(newBlog.url)
+    expect(result._body.likes).toBe(newBlog.likes)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(3)
+}, TIMEOUT)
 
 afterAll(async () => {
     await mongoose.connection.close()
