c46cb8f3c6b5b6e20c19c07b04e4a08c3d49a84b
katgur <mitsutanaka29@gmail.com>
2023-09-18 16:41:22 +0300

    4.9: Blog list tests, step2

--- part4/bloglist/test/blog_api.test.js
+++ part4/bloglist/test/blog_api.test.js
@@ -16,19 +16,23 @@ beforeEach(async () => {
     await blogObject.save()
 })
 
-describe('get blogs', () => {
-    test('blogs are returned as json', async () => {
-        await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-    }, TIMEOUT)
 
-    test('there are two blogs', async () => {
-        const response = await api.get('/api/blogs')
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}, TIMEOUT)
 
-        expect(response.body).toHaveLength(2)
-    })
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(2)
+})
+
+test('unique identifier is called id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+    expect(response.body[1].id).toBeDefined()
 })
 
 afterAll(async () => {
