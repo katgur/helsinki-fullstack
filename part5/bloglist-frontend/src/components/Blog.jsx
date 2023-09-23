cc611c0180f0896b2a27105bb6ce4722bb9ec3d5
katgur <mitsutanaka29@gmail.com>
2023-09-23 20:42:52 +0300

    5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isShown, setIsShown] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    borderBottom: 'solid 1px',
+  }
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title} {blog.author}
+        <button onClick={() => setIsShown(!isShown)}>{isShown ? 'hide' : 'show'}</button>
+      </p>
+      {isShown &&
+        <div>
+          <p><a href={blog.url}>{blog.url}</a></p>
+          <p>likes {blog.likes}</p>
+          <p>{blog.user.name}</p>
+        </div>}
+    </div>
+  )
+}
 
 export default Blog
