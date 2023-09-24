4912ee09428469ab4594c500c4c52d8c219fbfdf
katgur <mitsutanaka29@gmail.com>
2023-09-24 21:19:24 +0300

    5.14: Blog list tests, step2

--- part5/bloglist-frontend/test/Blog.test.js
+++ part5/bloglist-frontend/test/Blog.test.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../src/components/Blog'
 import mock from './mock/blogs'
 
-test('renders content', () => {
+test("doesn't show url and likes by default", () => {
     const blog = mock.blog
 
-    render(<Blog blog={blog} />)
+    const { container } = render(<Blog blog={blog} />)
 
-    const element = screen.getByText(`${blog.title} ${blog.author}`)
-    expect(element).not.toHaveTextContent(blog.url)
-    expect(element).not.toHaveTextContent(blog.likes)
+    expect(container).toHaveTextContent(`${blog.title} ${blog.author}`)
+    expect(container).not.toHaveTextContent(blog.url)
+    expect(container).not.toHaveTextContent(blog.likes)
+})
+
+test("show url and likes after show button click", async () => {
+    const blog = mock.blog
+    const user = userEvent.setup()
+
+    const { container } = render(<Blog blog={blog} />)
+
+    const button = screen.getByText('show')
+    await user.click(button)
+
+    expect(container).toHaveTextContent(`${blog.title} ${blog.author}`)
+    expect(container).toHaveTextContent(blog.url)
+    expect(container).toHaveTextContent(blog.likes)
 })
